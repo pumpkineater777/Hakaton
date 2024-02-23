1e1156cc1a93dae5ae12ad1f1b4e1a1ce9a3000b
Timur <melikov.th@students.sch2.ru>
2024-02-23 23:01:54 +0300

    main file

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ app = Flask(__name__)
 cur_id = 0
 data = []
 
-
-# hostname = cashback
-
 def stop_it(id):
     pass
 
@@ -17,11 +14,11 @@ def create_partner():
     global cur_id
     data.append({
         'id': cur_id,
-        'name': request.json['name'],
-        'budget': request.json['budget'],
+        'name': request.json["name"],
+        'budget': request.json["budget"],
         'spent_budget': 0,
         'is_stopped': False,
-        'data': []
+        'dates': []
     })
     cur_id += 1
     temp = dict()
@@ -35,19 +32,23 @@ def create_partner():
 @app.route('/api/partners/<int:id>', methods=['GET'])
 def get_info(id):
     temp = dict()
-    temp['id'] = data[-1]['id']
-    temp['name'] = data[-1]['name']
-    temp['budget'] = data[-1]['budget']
-    temp['spent_budget'] = data[-1]['spent_budget']
-    temp['is_stopped'] = data[-1]['is_stopped']
+    temp['id'] = data[id]['id']
+    temp['name'] = data[id]['name']
+    temp['budget'] = data[id]['budget']
+    temp['spent_budget'] = data[id]['spent_budget']
+    temp['is_stopped'] = data[id]['is_stopped']
     return jsonify(temp), 201
 
 
 @app.route('/api/partners/<int:id>/cashback', methods=['PUT'])
 def update_info(id):
     data[id]['spent_budget'] += request.json["cashback"]
-    data[id]['data'].append([request.json["data"].split()[0], request.json["cashback"]])
+    data[id]['dates'].append([request.json["date"].split()[0], request.json["cashback"]])
     stop_it(id)
+    return jsonify({"ok": "ok"}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
+#curl -i -H "Content-Type: application/json" -X POST --data "{"""name""":"""VTB""","""budget""":10002}" http://localhost:5000/api/partners
+#curl -i http://localhost:5000/api/partners/0
+#curl -i -H "Content-Type: application/json" -X PUT --data "{"""date""":"""2023-02-0200:00:00""","""name""":"""VTB""","""cashback""":23232.34}" http://localhost:5000/api/partners/0/cashback
