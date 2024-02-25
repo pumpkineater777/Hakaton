5e85062c2055841ca9750e1673dd9bc1046a3c12
Rizel1 <161014063+Rizel1@users.noreply.github.com>
2024-02-25 09:37:28 +0300

    Update Main.js

--- static/Main.js
+++ static/Main.js
@@ -18,7 +18,7 @@ function addOption(obj) {
 
     p.innerHTML = obj.name;
 
-    containerTwo.innerHTML = `										<svg width="30" height="30" class = "button ${obj.name}">
+    containerTwo.innerHTML = `										<svg width="30" idCompany = "${obj.id}" height="30" class = "button A${obj.name}">
 											<circle cx="18" cy="15" r="0.5" stroke="#e8e8e8" stroke-width="2" fill="#e8e8e8" />
 											<circle cx="15" cy="15" r="12" stroke="#e8e8e8" stroke-width="2" fill="none" />
 											<line x1="18" y1="15" x2="13" y2="9" style="stroke:#e8e8e8;stroke-width:3" />
@@ -27,21 +27,73 @@ function addOption(obj) {
 
     let parent = document.querySelector(".sections");
     parent.appendChild(option);
+    document.querySelector(`svg.button.A${obj.name}`).addEventListener("click", (e) => {
+        let idCompany = +e.target.getAttribute("idCompany");
 
-    document.querySelector(`svg.button.${obj.name}`).addEventListener("onclick", (e) => {
-      let dates = obj.dates;
-      let nav = document.querySelector("nav");
+        document.querySelector("nav").classList.toggle("hidden");
+        document.querySelector(".addPoint").setAttribute("idCompany", idCompany);
 
-      nav.classList.add("working");
+        fetch(`http://localhost:8080/api/partners/${idCompany}`, {method: 'GET', mod: "cors"})
+          .then((response) => {
+            if (!response.ok) {
+                 throw new Error(`HTTP error! Status: ${response.status}`);
+            }
 
-      console.log(dates);
+            return response.blob();
+             })
+          .then((response) => {
+            let graph = new Image();
+            graph.src = "../venv/graph.jpg"
 
-      draw(dates);
+            document.querySelector(".graph").style["background-image"] = `url('../venv/graph.jpg')`;
+
+           // let canvas = document.querySelector(".graph");
+            //let context = canvas.getContext("2d");
+            //context.drawImage(graph, 0, 0)
+          });
     });
 }
 
 function draw(data) {
 }
+
+document.querySelector(".addPoint").addEventListener("click", (e) => {
+
+   let idCompany = e.target.getAttribute("idCompany")
+   let cashback = document.querySelector("#Cashback").value
+   let day = document.querySelector("#Day").value
+
+    console.log(day)
+
+   if(cashback == "" || day == "")
+    return;
+
+    document.querySelector("#Cashback").value = ""
+    document.querySelector("#Day").value = ""
+
+    console.log({"date": day, "cashback": cashback})
+
+   fetch(`http://localhost:8080/api/partners/${idCompany}/cashback`, {method: 'PUT', headers: {'Content-Type': 'application/json;charset=utf-8'}, body: JSON.stringify({"cashback": cashback, "date": day})})
+          .then((response) => {
+            if (!response.ok) {
+                 throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            return response;
+             })
+          .then((response) => {
+            let graph = new Image();
+            graph.src = "../venv/graph.jpg"
+
+            console.log("OK")
+
+            document.querySelector(".graph").style["background-image"] = `url('../venv/graph.jpg')`;
+
+           // let canvas = document.querySelector(".graph");
+            //let context = canvas.getContext("2d");
+            //context.drawImage(graph, 0, 0)
+          });
+})
 
 document.querySelector(".icon").addEventListener("click", (e) => {
     e.stopPropagation();
@@ -87,9 +139,9 @@ document.querySelector("body").addEventListener("click", (e) => {
   .then((response) => {
     let data = response;
     let sections = document.querySelector(".sections");
-    console.log(data)
     for (x of data) {
        addOption(x);
     }
   });
 
+//////////////////////////////////
